2c93e87e881c4c3bc7ea1b07b9b4c5c8ddd8b0a2
502/stdnt-number-three <giybo6ix3q@yandex.ru>
2016-11-09 00:12:36 +0300

    [#2].default/script_structure.sh: Main loop added

--- .default/script_structure.sh
+++ .default/script_structure.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 helpstring="Script\nOlolo\n"
+welcome="==================\nС помощью данной программы вы можете %ACTIONS%.\n\nРазработчики: Иванов, Петров, Сидоров\n\nГлавное меню:\n\n1. Добавить пользователя\n2. Добавить группу\n3. Поиск пользователя\nq. Выход из программы"
 if [ ! -z "$(echo $@ | grep -- '--help')" ]; then 
 	echo -e "$helpstring"; exit 0; 
 fi
 
+
+while true;
+do
+	echo -e "$welcome"
+	read action
+	case $action in
+		"1")
+			echo "you chose choice #1"
+			;;
+		"2")
+			echo "you chose choice #2"
+                        ;;
+		"3")
+			echo "you chose choice #3"
+                        ;;
+		"q")
+			exit 0;
+                      	;;
+		*)
+			echo invalid option;;
+	esac
+done
+
+exit 0;
 
 S3='Please enter your choice: '
 options=("Option 1" "Option 2" "Option 3" "Quit")
