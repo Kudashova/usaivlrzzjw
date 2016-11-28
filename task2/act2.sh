37058b58e7abe539f4af7f9eee84d307cbb491d0
Kudashova <dessacrist@yandex.ru>
2016-11-28 20:52:36 +0300

    Реализовано задание 2

--- task2/act2.sh
+++ task2/act2.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 act2_1 () {
-echo ololo;
+	find / -perm -a+w -type f | less -XFR
 }
 
 main () {
-	echo plain_text;
+	actionlist1="Меню:\n1. Найти файлы, доступные на запись всем и вывести их список на экран.\nq. Вернуться назад"
+	helpstring1="[1] - выбор пункта для выполнения [q] - выход на уровень выше\n\n$actionlist1"
+	welcome1="==================\nВ данном меню можно:\nНайти файлы, доступные на запись всем и вывести их список на экран \n\n\n$actionlist1"
+	
+	echo -e "$welcome1"
+	while true;
+	do
+	        read action
+	        case $action in
+	                "1")
+	                        act2_1;
+	                        echo -e "\n$actionlist1"
+	                        ;;
+			"q")
+	                    	return
+	                        ;;
+			"help")
+				echo -e "$helpstring1"
+				read
+				echo -e "\n$actionlist1"
+				;;
+	                *)
+	                  	echo Нельзя!; echo -e "\n$actionlist1";
+	        esac
+	done
 }
 
+
 main "$@";
+
